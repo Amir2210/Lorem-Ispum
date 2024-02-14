9fce60ce9af6396edc418a4c92d4bcb77a7e0706
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-15 00:22:08 +0200

    Fix onSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
-
+import data from './data'
 const App = () => {
   const [pAmount, setPAmount] = useState(1)
-  function onChangePAmount(e) {
-    setPAmount(e.target.value)
-  }
+  const [text, setText] = useState([])
 
   function onSubmit(e) {
     e.preventDefault()
-    console.log(e)
+    setText(data.slice(0, pAmount))
+
   }
   return (
     <section className='section-center'>
       <h4>TIRED OF BORING LOREM IPSUM?</h4>
       <form className='lorem-form' onSubmit={onSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
-        <input onChange={onChangePAmount} type="number" value={pAmount} min={1} step={1} />
+        <input onChange={(e) => setPAmount(e.target.value)} type="number" value={pAmount} min={1} max={data.length - 1} step={1} name='pAmount' id='pAmount' />
         <button className='btn'>generate</button>
       </form>
+      <article className='lorem-text'>
+        {text.map(p => <p key={p}>{p}</p>)}
+      </article>
     </section>
   )
 }
