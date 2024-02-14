8b6d9ff6cb7fe4f5a4e87e2920816cd98cf34f82
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-14 23:51:59 +0200

    Add submit button to the form

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ const App = () => {
   function onChangePAmount(e) {
     setPAmount(e.target.value)
   }
+
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+  }
   return (
     <section className='section-center'>
       <h4>TIRED OF BORING LOREM IPSUM?</h4>
-      <form className='lorem-form'>
+      <form className='lorem-form' onSubmit={onSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
         <input onChange={onChangePAmount} type="number" value={pAmount} min={1} step={1} />
+        <button className='btn'>generate</button>
       </form>
     </section>
   )
