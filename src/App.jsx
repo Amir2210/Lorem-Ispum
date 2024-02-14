3ce96e3f30c44a3d8db903a5d55d37614fa44557
Amir Yankolovich <amiryankolovich2000@gmail.com>
2024-02-14 23:49:35 +0200

    Create basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useState } from 'react'
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>
+  const [pAmount, setPAmount] = useState(1)
+  function onChangePAmount(e) {
+    setPAmount(e.target.value)
+  }
+  return (
+    <section className='section-center'>
+      <h4>TIRED OF BORING LOREM IPSUM?</h4>
+      <form className='lorem-form'>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input onChange={onChangePAmount} type="number" value={pAmount} min={1} step={1} />
+      </form>
+    </section>
+  )
 }
 export default App
